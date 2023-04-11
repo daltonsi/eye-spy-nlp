f04d75f9049fbb9e254b6551d84a3c781f157074
Dalton Simancek <daltonsi@daltonsi-mbp.local>
2023-04-10 20:52:28 -0400

    cataract labeling

--- preprocess_kaggle.py
+++ preprocess_kaggle.py
@@ -1,5 +1,5 @@
 import pandas as pd 
-
+import numpy as np
 
 SOURCE_PATH = 'source_data/mtsamples.csv'
 
@@ -7,11 +7,23 @@ SOURCE_PATH = 'source_data/mtsamples.csv'
 
 
 def main():
-"""preprocess source data to Opthamal speciality"""
-	df = pd.read_csv(SOURCE_PATH)
+	"""preprocess source data to Opthamal speciality"""
+	df = pd.read_csv(SOURCE_PATH, index_col=0)
 	df_stripped = df.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
 	opthamal_mask = df_stripped['medical_specialty'] == 'Ophthalmology'
 	opthamal_df = df[opthamal_mask]
+
+	conditions = [
+		(opthamal_df['sample_name'].str.contains('Cataract')==True)
+	]
+
+	values = ['cataract']
+
+
+	opthamal_df['label'] = np.select(conditions,values)
+
+
+
 	opthamal_df.to_csv('source_data/mtsamples_opthamal.csv')
 
 
