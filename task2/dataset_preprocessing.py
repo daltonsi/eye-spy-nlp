7559edc0476c31e0b8043d93afc5280cf0c1a7f2
Dalton Scott Simancek <daltonsi@gl-login2.arc-ts.umich.edu>
2023-04-20 13:25:15 -0400

    + test and predict

--- task2/dataset_preprocessing.py
+++ task2/dataset_preprocessing.py
@@ -1,177 +1,125 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torch.utils.data.dataset import random_split
+import torch.utils.data as data
+
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, \
+    TrainingArguments, Trainer
+
+from datasets import load_dataset, load_metric
+
+
 import pandas as pd
-import re
-from collections import Counter
+import numpy as np
+import os
+import time
 
-SOURCE_PATH = '/home/daltonsi/eye-spy-nlp/source_data/mtsamples_opthamal.csv'
+DATA_PATH = '/Users/daltonsi/HILS/LHS712/project/source_data/task2_source.csv'
+UNIQUE_LABELS = ["ALLERGIES", "ANESTHESIA", "COMPLICATIONS", "DIAGNOSIS", "HISTORY", "MEDICATION","OBJECTIVE","OTHER","PLAN","PROCEDURE","SUBJECTIVE"]
 
-
-
-LABEL_MAPPINGS = {
-    'SUBJECTIVE': 'SUBJECTIVE',
-    'CHIEF COMPLAINT': 'SUBJECTIVE',
-    'REASON FOR VISIT': 'SUBJECTIVE',
-    'HISTORY OF PRESENT ILLNESS': 'SUBJECTIVE',
-    'PLAN': 'PLAN',
-    'PLANS': 'PLAN',
-    'ASSESSMENT': 'PLAN',
-    'IMPRESSION': 'PLAN',
-    'ANESTHESIA': 'ANESTHESIA',
-    'ANESTHESIA TYPE': 'ANESTHESIA',
-    'OBJECTIVE': 'OBJECTIVE',
-    'EYE EXAM': 'OBJECTIVE',
-    'PHYSICAL EXAMINATION': 'OBJECTIVE',
-    'OCULAR FINDINGS': 'OBJECTIVE',
-    'VISUAL ACUITY': 'OBJECTIVE',
-    'FINDINGS': 'OBJECTIVE',
-    'IMPLANTS': 'OBJECTIVE',
-    'HISTORY': 'HISTORY',
-    'DOB': 'HISTORY',
-    'FAMILY HISTORY': 'HISTORY',
-    'SOCIAL HISTORY': 'HISTORY',
-    'PAST SURGICAL HISTORY': 'HISTORY',
-    'PAST OCULAR HISTORY': 'HISTORY',
-    'PAST MEDICAL HISTORY': 'HISTORY',
-    'MEDICATION': 'MEDICATION',
-    'MEDICATION HISTORY': 'MEDICATION',
-    'MEDICATIONS': 'MEDICATION',
-    'ALLERGIES': 'ALLERGIES',
-    'PROCEDURE': 'PROCEDURE',
-    'PROCEDURES': 'PROCEDURE',
-    'OPERATIVE PROCEDURE': 'PROCEDURE',
-    'OPERATIONS PERFORMED': 'PROCEDURE',
-    'OPERATIVE TECHNIQUE': 'PROCEDURE',
-    'OPERATIVE PROCEDURES': 'PROCEDURE',
-    'TITLE OF OPERATION': 'PROCEDURE',
-    'PROCEDURE PERFORMED': 'PROCEDURE',
-    'PROCEDURE IN DETAIL': 'PROCEDURE',
-    'DESCRIPTION OF OPERATION': 'PROCEDURE',
-    'PROCEDURE DETAILS': 'PROCEDURE',
-    'DESCRIPTION OF THE OPERATION': 'PROCEDURE',
-    'DESCRIPTION OF PROCEDURE': 'PROCEDURE',
-    'OPERATIVE PROCEDURE IN DETAIL': 'PROCEDURE',
-    'OPERATION': 'PROCEDURE',
-    'PROCEDURE NOTE': 'PROCEDURE',
-    'OPERATION PERFORMED': 'PROCEDURE',
-    'NAME OF PROCEDURE': 'PROCEDURE',
-    'COMPLICATIONS': 'COMPLICATIONS',
-    'DIAGNOSIS': 'DIAGNOSIS',
-    'INDICATIONS FOR OPERATION': 'DIAGNOSIS',
-    'INDICATIONS FOR PROCEDURE': 'DIAGNOSIS',
-    'PREOPERATIVE DIAGNOSIS': 'DIAGNOSIS',
-    'PREOPERATIVE DX': 'DIAGNOSIS',
-    'INDICATIONS FOR SURGERY': 'DIAGNOSIS',
-    'PREOP DIAGNOSIS': 'DIAGNOSIS',
-    'POSTOPERATIVE DX': 'DIAGNOSIS',
-    'POSTOPERATIVE DIAGNOSIS': 'DIAGNOSIS',
-    'PREOPERATIVE DIAGNOSES': 'DIAGNOSIS',
-    'PREOPERATIVE FINDINGS': 'DIAGNOSIS',
-    'INDICATION': 'DIAGNOSIS',
-    'INDICATIONS': 'DIAGNOSIS',
-    'POSTOP DIAGNOSIS': 'DIAGNOSIS',
-    'POSTOPERATIVE DIAGNOSES': 'DIAGNOSIS',
-    'INDICATION FOR SURGERY': 'DIAGNOSIS',
-    'OTHER': 'OTHER',
-    'BLOOD LOSS': 'OTHER',
-    'ESTIMATED BLOOD LOSS': 'OTHER',
-    'OD': 'OTHER',
-    'RE': 'OTHER',
-    'IOL': 'OTHER',
-    'XYZ': 'OTHER',
-    'OS': 'OTHER',
-    'OU': 'OTHER',
-    'SPECIMEN': 'OTHER',
-    'SPECIMENS': 'OTHER',
-    'PMH': 'OTHER',
-    'IOP': 'OTHER',
-    'REVIEW OF SYSTEMS': 'OTHER',
-    'PATIENT INSTRUCTIONS': 'OTHER',
-    'PHACOEMULSIFICATION TIME': 'OTHER',
-    'INFORMED CONSENT': 'OTHER',
-    'LENS IMPLANT USED': 'OTHER',
-    'DISCHARGE': 'OTHER',
-    'PHACO TIME': 'OTHER',
-    'CONDITION': 'OTHER',
-    'TEST RESULTS': 'OTHER',
-    'INTRAOCULAR LENS': 'OTHER',
-    'TO PREVENT THE SPREAD OF THE INFECTION': 'OTHER',
-    'REFRACTION': 'OTHER',
-    'ASSISTANT': 'OTHER',
-    'NARRATIVE': 'OTHER',
-    'PROS DEV IMPLANT': 'OTHER',
-}
+# Train Parameters
+lr = 1e-2
+batch_size = 16
+run_name = "LHS712-task2"
+num_epochs = 1
+logging_steps = 5000
+save_total_limit = 1
+seed = 1
+out_dir = '/home/daltonsi/eye-spy-nlp/task2/output'
+model_name = 'bert-base-cased'
+resume_from_checkpoint = False
 
 
-def parse_headings(data_path:str):
+def tokenize_data(df):
+    tokenizer = AutoTokenizer.from_pretrained('bert-base-cased')
 
-    headings = []
-
-    df = pd.read_csv(data_path, index_col=0)
-
-    # Retrieve all header in source data
-    for row in df['transcription'].iloc[:].items():
-        try:
-            idx, text = row
-
-            reg_headers = r'[A-Z][A-Z ]+:'
-
-            headings += re.findall(reg_headers, text)
-        except:
-            print(row)
-
-    print(Counter([i.strip(':') for i in headings]))
-    return list(i.strip(':') for i in set(headings))
+    return tokenizer(df['text'], padding='max_length', truncation=True)
 
 
-def create_dataset(data_path:str, labels: {}):
+def preprocess(df):
+    # tweets = df['Tweet']
+    # tod = df['TimeOfDay']
+    df['tokens'] = df.apply(tokenize_data, axis=1)
+    return df
 
-    df = pd.read_csv(SOURCE_PATH, index_col=0)
+def transform_labels(label):
 
-    data = []
+    label = label['label']
+    num = 0
+    if label == 'ALLERGIES':
+        num = 0
+    elif label == 'ANESTHESIA':
+        num = 1
+    elif label == 'COMPLICATIONS':
+        num = 2
+    elif label == 'DIAGNOSIS':
+        num = 3
+    elif label == 'HISTORY':
+        num = 4
+    elif label == 'MEDICATION':
+        num = 5
+    elif label == 'OBJECTIVE':
+        num = 6
+    elif label == 'OTHER':
+        num = 7
+    elif label == 'PLAN':
+        num = 8
+    elif label == 'PROCEDURE':
+        num = 9
+    elif label == 'SUBJECTIVE':
+        num = 10
+    return {'labels': num}
 
-    for row in df['transcription'].iloc[:].items():
-        idx, text = row
-
-        pattern = r'([A-Z][A-Z ]+):(.*?)(?=[A-Z][A-Z ]+:|$)'
-        none_pattern = r'(?<![^\W\d])None[.,](?![^\W\d])'
-
-        matches = re.findall(pattern, text)
-        for match in matches:
-            pre_label, text = match
-            label = labels[pre_label]
-
-            if re.search(none_pattern, text) == None:
-
-                data.append([label,text])
-
-    df = pd.DataFrame(data)
-    df.columns = ['label','text']
-    df.to_csv('/home/daltonsi/eye-spy-nlp/source_data/task2_source.csv')
 
 def main():
-    #labels = parse_headings(SOURCE_PATH)
-    data = create_dataset(SOURCE_PATH,LABEL_MAPPINGS)
+    task2_df = pd.read_csv(DATA_PATH,header=0,usecols=['label','text']).dropna()
+    task2_df.reset_index().to_csv('task2_full_data.csv')
+    dataset = load_dataset('csv', data_files={'train': 'task2_full_data.csv'}, split='train')
 
 
 
 
+    remove_columns = ['index', 'Unnamed: 0','label']
 
-    # df_stripped = df.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
-	# opthamal_mask = df_stripped['medical_specialty'] == 'Ophthalmology'
-	# opthamal_df = df[opthamal_mask]
-    #
-	# conditions = [
-	# 	(opthamal_df['sample_name'].str.contains('Cataract')==True)
-	# ]
-    #
-	# values = ['cataract']
-    #
-    #
-	# opthamal_df['label'] = np.select(conditions,values)
-    #
-    #
-    #
-	# opthamal_df.to_csv('source_data/mtsamples_opthamal.csv')
+    tokenizer = AutoTokenizer.from_pretrained('bert-base-cased')
 
+    def tokenize_data(df):
+        return tokenizer(df['text'], padding='max_length', truncation=True)
+
+    dataset = dataset.map(tokenize_data, batched=True)
+    dataset = dataset.map(transform_labels, remove_columns=remove_columns)
+
+
+
+    training_args = TrainingArguments("test_trainer", num_train_epochs=1, logging_steps=100)
+
+    model = AutoModelForSequenceClassification.from_pretrained('bert-base-cased', num_labels=11)
+    train_dataset = dataset.shuffle(seed=10).select(range(203))
+    eval_dataset = dataset.shuffle(seed=10).select(range(203, 271))
+    test_dataset = dataset.shuffle(seed=10).select(range(271, 339))
+
+    trainer = Trainer(
+        model=model, args=training_args, train_dataset=train_dataset, eval_dataset=eval_dataset
+    )
+    trainer.train()
+
+
+    metric = load_metric("accuracy")
+
+    def compute_metrics(eval_pred):
+        logits, labels = eval_pred
+        predictions = np.argmax(logits, axis=-1)
+        return metric.compute(predictions=predictions, references=labels)
+
+    trainer.evaluate()
+
+
+    predictions = trainer.predict(test_dataset['train'])
+    y_pred_list = list(np.argmax(predictions.predictions, axis=-1))
+
+    return None
 
 
 
