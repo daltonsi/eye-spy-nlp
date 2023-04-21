fcfbb4ae81d5943211480d43be758e7c85ea0db2
Shreya Kapoor <89872698+Shreyakapoor26@users.noreply.github.com>
2023-04-21 18:15:56 -0400

    first commit
    Looks for ophthalmology diseases based on CUI and extract lines

---
+++ look_up.py
@@ -0,0 +1,24 @@
+####################
+# By- Shreya Kapoor
+####################
+
+with open('fp_mappings.out', 'r') as file:
+    for line in file:
+        data = line.strip().split('|')
+        try:
+            fourth_column_data = data[4]
+        except IndexError:
+            continue
+
+        with open('disease_names_with_CUI', 'r') as lookup_file:
+            for lookup_line in lookup_file:
+                lookup_data = lookup_line.strip().split(',')
+                try:
+                    Ophthalmology_CUI_lookup = lookup_data[1]
+                except IndexError:
+                    continue
+
+                if Ophthalmology_CUI_lookup == fourth_column_data:
+                    with open('matching_rows.out', 'a') as output_file:
+                        output_file.write(line)
+                    break
